6de53a6195d1ed9f3e9128c97648d3766061ff9b
Paloque-Berges <paloque.jack@gmail.com>
2016-09-15 13:08:50 +0200

    Update fr-help.php

--- fr-help.php
+++ fr-help.php
@@ -6,9 +6,7 @@ Le principe du plugin plux-automatic est de copier une archive dans un répertoi
 
 Grâce au plugin, vous donnerez un nom à vos sites de test (toujours un seul mot, majuscules et tirets acceptés) et  pourrez les supprimer quand vous n'en aurez plus besoin. Tout se fait tout seul. ;)<br /><br />
 
-<strong>Problème connu</strong><br /><br />
 
-<p> Sous Firefox, le clic sur une case à cocher ouvre le site mais reste cochée. Ce bug ne se produit pas avec Chrome. </p>
 
 <p> Je vous souhaite de trés bons tests et bon vent. :)</p>
 
